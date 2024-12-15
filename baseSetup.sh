a1950b4fd569e4972a98288fbbc908083f77037a
Mikhail Yashkur <61008548+YAMISHKA02@users.noreply.github.com>
2024-12-15 21:40:47 +0300

    Update baseSetup.sh

--- baseSetup.sh
+++ baseSetup.sh
@@ -3,23 +3,20 @@
 # Функция для настройки iptables
 setup_iptables() {
     echo "Загружается и выполняется скрипт для настройки iptables..."
-    bash <(curl -H "Authorization: token " \
-     -s https://raw.githubusercontent.com/YAMISHKA02/NodeGoods/refs/heads/main/NewServer/iptables.sh)
+    bash <(curl -s https://raw.githubusercontent.com/YAMISHKA02/NodeScripts/refs/heads/main/iptable.sh)
     echo "Настройка iptables завершена."
 }
 
 # Функция для установки Fail2ban
 install_fail2ban() {
     echo "Загружается и выполняется скрипт для установки Fail2ban..."
-    bash <(curl -H "Authorization: token " \
-     -s https://raw.githubusercontent.com/YAMISHKA02/NodeGoods/refs/heads/main/NewServer/file2ban.sh)
+    bash <(curl -s https://raw.githubusercontent.com/YAMISHKA02/NodeScripts/refs/heads/main/file2ban.sh)
     echo "Установка Fail2ban завершена."
 }
 
 install_docker() {
     echo "Загружаем Docker"
-    bash <(curl -H "Authorization: token " \
-     -s https://raw.githubusercontent.com/YAMISHKA02/NodeGoods/refs/heads/main/NewServer/docker.sh)
+    bash <(curl -s https://raw.githubusercontent.com/YAMISHKA02/NodeScripts/refs/heads/main/docker.sh)
     echo "Установка Docker завершена."
 }
 
@@ -29,9 +26,7 @@ handle_choice() {
     case "$1" in
         1) setup_iptables ;;
         2) install_fail2ban ;;
-        3) install_docker ;;
-        #4) clear_memory ;; 
-        #9) node_list_start ;;               
+        3) install_docker ;;             
         0) echo "Выход."; exit 0 ;;
         *) echo "Неверный выбор. Попробуйте снова." ;;
     esac
